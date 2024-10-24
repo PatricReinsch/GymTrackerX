3e8e0eb4ee8d29f7644249beac0482b0f456259d
Patric Reinsch <Patric.reinsch@hotmail.com>
2024-10-24 14:53:04 +0200

    Updated home_screen.dart and pubspec.yaml

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../utils/icons.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,13 +8,31 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('GymTrackerX')),
-      body: Center(
-        child: TextButton(
-          child: const Text('Click Me!'), // Corrected: Wrap the text in a Text widget
-          onPressed: () {
-            // Handle button click
-          },
+      appBar: AppBar(
+        toolbarHeight: 150,
+        title: SvgPicture.asset(
+          'assets/images/logo/svg/logo-no-background.svg',
+          height: 65,
+          fit: BoxFit.cover,
+        ),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 10),
+            Text(
+              "Create your first Plan",
+              style: TextStyle(
+                fontSize: 30, // Change this value to adjust font size
+                fontWeight: FontWeight.bold, // Optional: Make text bold
+                color: Colors.black, // Optional: Change text color
+              ),
+            ),
+            SizedBox(height: 20),
+            Icon(CustomIcons.addCircleOutlineRounded, size: 60),
+            // Custom icon
+          ],
         ),
       ),
     );
---
+++ lib/utils/icons.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData addCircleOutlineRounded = IconData(0xf52c, fontFamily: 'MaterialIcons');
+}
