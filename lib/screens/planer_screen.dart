6abdf444c913130658e228f6b6d91ae3fc921dce
PatricReinsch <patric.reinsch@hotmail.com>
2024-11-14 11:06:02 +0100

    Start Implementing planer_screen

--- lib/screens/planer_screen.dart
+++ lib/screens/planer_screen.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // Make sure to import this package
 
 class PlanerScreen extends StatelessWidget {
   const PlanerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 150,
+        title: SvgPicture.asset(
+          'assets/images/logo/svg/logo-no-background.svg',
+          height: 65,
+          fit: BoxFit.cover,
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: Colors.black,
+              width: 4,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: const Text(
+            'Content goes here', // Placeholder text within the container
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+    );
   }
 }
