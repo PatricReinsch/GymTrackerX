4f6150aa7341e9f442e57e5d7e4ed9eafe84a405
Carina Bastron <carina.bastron@gmail.com>
2024-11-18 09:07:07 +0100

    added split page

--- lib/screens/split_screen.dart
+++ lib/screens/split_screen.dart
@@ -1,11 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'planer_screen.dart';
 
 class SplitScreen extends StatelessWidget {
   const SplitScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 150,
+        title: SvgPicture.asset(
+          'assets/images/logo/svg/logo-no-background.svg',
+          height: 65,
+          fit: BoxFit.cover,
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Select your Split",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Fullbody Split",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Slider(
+              value: 0,
+              min: 0,
+              max: 1,
+              divisions: 1,
+              onChanged: null,
+            ),
+            const SizedBox(height: 40),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const PlanerScreen()),
+                );
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  "Next",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
