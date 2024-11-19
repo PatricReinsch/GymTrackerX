2c2c8e981179cbc6c0361947f0a7a347d377b557
OrlandoLegolas <thanheiser@siemens.com>
2024-11-19 18:19:04 +0100

    excersise-screen methode in main fail changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'GymTrackerX',
       home: LoginScreen(),
-      routes: {
-        '/exercisescreen': (context) => ExerciseScreen(),
-      },
     );
   }
 }
