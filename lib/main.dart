6e9962a6ead1aff5b6602f5ba18077e2f7a90179
PatricReinsch <patric.reinsch@hotmail.com>
2024-12-11 14:52:58 +0100

    Added Handout

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:gym_tracker_x/screens/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Main());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GymTrackerX',
-      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  CounterScreenState createState() => CounterScreenState();
+}
+
+class CounterScreenState extends State<CounterScreen> {
+  int _counter = 0; // Zähler-Variable
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Einfacher Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Du hast den Button so oft geklickt:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
