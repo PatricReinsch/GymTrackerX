57a16c59a4662c0895a6a4854e3dc888d7f80b6b
oskarkreutzer <oskarkreutzer@mail.de>
2025-05-05 18:07:24 +0200

    added ui test for login_screen

--- test/ui/login_screen_test.dart
+++ test/ui/login_screen_test.dart
@@ -5,39 +5,39 @@ import 'package:gym_tracker_x/widgets/custom_button_black.dart';
 
 void main() {
   testWidgets('LoginScreen displays fields and responds correctly to entries',
-      (WidgetTester tester) async {
-    // render LoginScreen
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: LoginScreen(),
-      ),
-    );
+          (WidgetTester tester) async {
+        // render LoginScreen
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: LoginScreen(),
+          ),
+        );
 
-    // find text boxes
-    final usernameField = find.widgetWithText(TextField, 'Username');
-    final passwordField = find.widgetWithText(TextField, 'Password');
-    final loginButton = find.byType(CustomButtonBlack);
+        // find text boxes
+        final usernameField = find.widgetWithText(TextField, 'Username');
+        final passwordField = find.widgetWithText(TextField, 'Password');
+        final loginButton = find.byType(CustomButtonBlack);
 
-    // check existency
-    expect(usernameField, findsOneWidget);
-    expect(passwordField, findsOneWidget);
-    expect(loginButton, findsOneWidget);
+        // check existency
+        expect(usernameField, findsOneWidget);
+        expect(passwordField, findsOneWidget);
+        expect(loginButton, findsOneWidget);
 
-    // Test case: Empty fields => should display errors
-    await tester.tap(loginButton);
-    await tester.pump();
+        // Test case: Empty fields => should display errors
+        await tester.tap(loginButton);
+        await tester.pump();
 
-    expect(find.text('Invalid username or password'), findsOneWidget);
+        expect(find.text('Invalid username or password'), findsOneWidget);
 
-    // Test case: Entering data
-    await tester.enterText(usernameField, 'tester');
-    await tester.enterText(passwordField, '123456');
+        // Test case: Entering data
+        await tester.enterText(usernameField, 'tester');
+        await tester.enterText(passwordField, '123456');
 
-    expect(find.text('tester'), findsOneWidget);
-    expect(find.text('123456'), findsOneWidget);
+        expect(find.text('tester'), findsOneWidget);
+        expect(find.text('123456'), findsOneWidget);
 
-    // press login again
-    await tester.tap(loginButton);
-    await tester.pump(); // pump() required for async behavior
-  });
+        // press login again
+        await tester.tap(loginButton);
+        await tester.pump(); // pump() required for async behavior
+      });
 }
